200b7ff4b696d679f822599d8ea02dbe5f867eb1
Chhayank Sharma <93913184+ChhayankCreator@users.noreply.github.com>
2021-11-10 22:35:12 +0530

    Load Sample data

---
+++ Quizzes/script.js
@@ -0,0 +1,36 @@
+// Sample data
+const quizDB = [{
+        question: "Q1: What is the full form of HTML?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "HyperText Makeup Language",
+        d: "Hypertext Markup Language",
+        ans: "ans4"
+    },
+    {
+        question: " Q2: What is the full form of CSS?",
+        a: "Cascading Style Sheets",
+        b: "Cascading Style Sheep",
+        c: "Cartoon Style Sheets",
+        d: "Cascading Super Sheets",
+        ans: "ans1"
+    },
+
+    {
+        question: " Q3: What is the full form of HTTP?",
+        a: "Hypertext Transfer Product",
+        b: "Hypertext Test Protocol",
+        c: "Hey Transfer Protocol",
+        d: "Hypertext Transfer Protocol",
+        ans: "ans4"
+    }, {
+        question: "Q4: What is the full form of JS?",
+        a: "JavaScript",
+        b: "JavaSuper",
+        c: "JustScript",
+        d: "JordenShoes",
+        ans: "ans1"
+    }
+];
+
+// Load Questions
