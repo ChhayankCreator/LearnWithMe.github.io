5784763db6748b057ee0fd784196e3ff936c3b24
Chhayank Sharma <93913184+ChhayankCreator@users.noreply.github.com>
2021-11-10 22:36:13 +0530

    Load Questions From sample data to screen

--- Quizzes/script.js
+++ Quizzes/script.js
@@ -34,3 +34,50 @@ const quizDB = [{
 ];
 
 // Load Questions
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const submit = document.querySelector('#submit');
+const answers = document.querySelectorAll('.answer');
+const showScore = document.querySelector('#showScore');
+
+let questionCount = 0;
+let score = 0
+
+
+const loadQuestion = () => {
+
+    const questionList = quizDB[questionCount];
+
+    question.innerText = questionList.question;
+
+    option1.innerText = questionList.a;
+    option2.innerText = questionList.b;
+    option3.innerText = questionList.c;
+    option4.innerText = questionList.d;
+
+}
+
+const deselectAll = () => {
+    answers.forEach(currCheckedAns => currCheckedAns.checked = false);
+}
+
+
+loadQuestion();
+
+const getCheckAnswer = () => {
+
+    let answer;
+
+    answers.forEach(curAnsElem => {
+        if (curAnsElem.checked) {
+            answer = curAnsElem.id
+        }
+    })
+
+    return answer;
+
+}
