cc608ef0090a5c92e7fa5626d42bcd98d75a70b6
Amarendra Saha <92846512+Amarendra-Saha@users.noreply.github.com>
2021-11-11 16:02:18 +0530

    Added selector js

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/**
+* Template Name: Mentor - v4.6.0
+* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+    "use strict";
+  
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
