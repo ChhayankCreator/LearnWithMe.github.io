9825a626c2fa4ccb6c7cfd67e562e816cb093d5e
Chhayank Sharma <93913184+ChhayankCreator@users.noreply.github.com>
2021-11-14 16:45:43 +0530

    added question js  for cn

---
+++ Quizzes/Quizzes/js/cnquestions.js
@@ -0,0 +1,171 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 2,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 4,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 5,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 6,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 7,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 8,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 9,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 10,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 11,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 12,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 13,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 14,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }, {
+        numb: 15,
+        question: "",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+
+
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
