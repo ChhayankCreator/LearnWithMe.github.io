80b48cbef9ea0845a07351117910a23052acf396
Chhayank Sharma <93913184+ChhayankCreator@users.noreply.github.com>
2021-11-15 23:58:36 +0530

    Added Data To CSS.js

---
+++ Quizzes/Quizzes/js/cssquestions.js
@@ -0,0 +1,170 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "The property in CSS used to change the background color of an element is -",
+        answer: "background-color",
+        options: [
+            "bgcolor",
+            "color",
+            "background-color",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The property in CSS used to change the text color of an element is -",
+        answer: "color",
+        options: [
+            "bgcolor",
+            "color",
+            "background-color",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "The CSS property used to control the element's font-size is -",
+        answer: "font-size",
+        options: [
+            "text-style",
+            "text-size",
+            "font-size",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The HTML attribute used to define the inline styles is -",
+        answer: "style",
+        options: [
+            "style",
+            "styles",
+            "class",
+            "None of the above"
+        ]
+    }, {
+        numb: 5,
+        question: "The HTML attribute used to define the internal stylesheet is -",
+        answer: "<style>",
+        options: [
+            "<style>",
+            "style",
+            "<link>",
+            "<script>"
+        ]
+    }, {
+        numb: 2,
+        question: " Which of the following CSS property is used to set the background image of an element?",
+        answer: "background-image",
+        options: [
+            "background-attachment",
+            "background-image",
+            "background-color",
+            "None of the above"
+        ]
+    }, {
+        numb: 2,
+        question: "Which of the following property is used as the shorthand property for the padding properties?",
+        answer: "padding",
+        options: [
+            "padding-left",
+            "padding-right",
+            "padding",
+            "All of the above"
+        ]
+    }, {
+        numb: 2,
+        question: "The CSS property used to make the text bold is -",
+        answer: "font-weight : bold",
+        options: [
+            "font-weight : bold",
+            "weight: bold",
+            "font: bold",
+            "style: bold"
+        ]
+    }, {
+        numb: 2,
+        question: "Are the negative values allowed in padding property?",
+        answer: "No",
+        options: [
+            "Yes",
+            "No",
+            "both",
+            "May be"
+        ]
+    }, {
+        numb: 2,
+        question: "Which of the following property is used as the shorthand property of margin properties?",
+        answer: "margin",
+        options: [
+            "margin L",
+            "margin R",
+            "margin",
+            "None Of Above"
+        ]
+    }, {
+        numb: 2,
+        question: "The CSS property used to specify the transparency of an element is -",
+        answer: "opacity",
+        options: [
+            "opacity",
+            "filter",
+            "visibility",
+            "overlay"
+        ]
+    }, {
+        numb: 2,
+        question: "Which of the following CSS property is used to specify the space between every letter inside an element?",
+        answer: "letter-spacing",
+        options: [
+            "alpha-spacing",
+            "character-spacing",
+            "letter-spacing",
+            "alphabet-spacing"
+        ]
+    }, {
+        numb: 2,
+        question: " The CSS property used to specify whether the text is written in the horizontal or vertical direction?",
+        answer: "writing-mode",
+        options: [
+            "writing-mode",
+            "text-indent",
+            "word-break",
+            "None of the above"
+        ]
+    }, {
+        numb: 2,
+        question: " How to select the elements with the class name example?",
+        answer: ".example",
+        options: [
+            "example",
+            "#example",
+            ".example",
+            "Class example"
+        ]
+    }, {
+        numb: 2,
+        question: " The CSS property used to draw a line around the elements outside the border?",
+        answer: "outline",
+        options: [
+            "border",
+            "outline",
+            "padding",
+            "line"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
