2f538b69e6b51964524e893edf783574afe4003e
Chhayank Sharma <93913184+ChhayankCreator@users.noreply.github.com>
2021-11-23 00:26:41 +0530

    Added Password reset function to firebase auth

--- index.js
+++ index.js
@@ -54,3 +54,23 @@ function login() {
             getElementById("error").innerHTML = error.message
         })
 }
+
+function signUp() {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch((error) => {
+            document.getElementById("error").innerHTML = error.message
+        });
+}
+
+function forgotPass() {
+    const email = document.getElementById("email").value
+    firebase.auth().sendPasswordResetEmail(email)
+        .then(() => {
+            alert("Reset link sent to your email id")
+        })
+        .catch((error) => {
+            document.getElementById("error").innerHTML = error.message
+        });
+}
